010f3a123c667e9e32997fcc61af00b20aa591d8
Pushpak Jaiswal <149317014+PUSHPAK-JAISWAL@users.noreply.github.com>
2026-09-21 20:50:04 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+  compileSdk { version = release(37) }
 
   defaultConfig {
     applicationId = "com.aistudio.gatepapers.pushpak"
